5cd7075fcd58d1bbed3ddf823f00780a54bd9198
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2024-07-31 16:20:18 +0700

    DONE : Template Show

--- web/ecommerce/views.py
+++ web/ecommerce/views.py
@@ -4,3 +4,6 @@ from .modules.auth.login import login_view
 # Create your views here.
 def login(request):
     return login_view(request)
+
+def dashboard(request):
+    return render(request, 'templates/dashboard.html')
