7e306728c94a90380c27a52d96ed31483c82f881
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2024-08-01 09:08:22 +0700

    DONE : Master Product View

---
+++ web/ecommerce/modules/master/product.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def product_view(request):
+    data = {
+        'title': 'Master Product'
+    }
+    
+    return render(request, 'product/display.html', data)
--- web/ecommerce/views.py
+++ web/ecommerce/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from .modules.auth.login import login_view
+from .modules.master.product import product_view
 
-# Create your views here.
+def dashboard(request):
+    data = {
+        'title': "Dashboard"
+    }
+    
+    return render(request, 'template/dashboard.html', data)
+
+# Auth
 def login(request):
     return login_view(request)
 
-def dashboard(request):
-    return render(request, 'templates/dashboard.html')
+# Master
+def product(request):
+    return product_view(request)
